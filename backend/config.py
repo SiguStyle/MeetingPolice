e03b159facdbe9e44b176916776bdeefd6b87317
SiguStyle <sigustyle@gmail.com>
2025-11-10 22:14:38 +0900

    Fix BaseSettings import for pydantic v2

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,8 @@ from functools import lru_cache
 from pathlib import Path
 from typing import List
 
-from pydantic import BaseSettings, AnyHttpUrl
+from pydantic import AnyHttpUrl
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
